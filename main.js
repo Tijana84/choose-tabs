3f829e864945cceab85b7b34f231bd7551fe28e9
Tijana Savic <tijanasavic1984@gmail.com>
2022-09-26 13:44:10 +0200

    choose tabs

---
+++ main.js
@@ -0,0 +1,15 @@
+let tabs = document.querySelectorAll(".tabs_simple");
+let contents = document.querySelectorAll(".tabs_content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabs.forEach((el) => {
+      el.classList.remove("active");
+    });
+    tabs[index].classList.add("active");
+    contents[index].classList.add("active");
+  });
+});
